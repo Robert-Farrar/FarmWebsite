03ff0cb8413dcb2a2440123bb659940f6b79b22e
Tony Aldana <125922681+AldanaTA@users.noreply.github.com>
2025-03-08 17:21:49 -0600

    Update StoreContoller.php
    Forgot to do update did Update

--- StoreContoller.php
+++ StoreContoller.php
@@ -1,30 +1,49 @@
 
 <?php
-class Store{
+class Item{
+    private $storeItemID = null;
     public $storeID = 0;
-    public $storeLocation = '';
-  
-    function __construct($storeID,$storeLocation){
+    public $itemID = 0;
+    public $inStock = '';
+    public $storeItemQuantity = 0;
+    function __construct($storeID,$itemID,$inStock,$storeItemQuantity){
         $this->storeID = $storeID;
-        $this->storeLocation = $storeLocation;
-    
+        $this->itemID = $itemID;
+        $this->inStock = $inStock;
+        $this->storeItemQuantity = $storeItemQuantity;
     }    
     public function toJson(){
+        #$data = array('storeItemID' => null, 'storeID' => $this->storeID, 'itemID' => $this->itemID, 'inStock' => $this -> inStock, 'storeItemQuantity' => $this -> storeItemQuantity);
         $json = json_encode($this);
         return $json;
     }
 }
-class StoreController{
+class UpdateItem{
+    public $storeID = 0;
+    public $itemID = 0;
+    public $storeItemQuantity = 0;
+    function __construct($storeID,$itemID,$storeItemQuantity){
+        $this->storeID = $storeID;
+        $this->itemID = $itemID;
+        $this->storeItemQuantity = $storeItemQuantity;
+    }    
+    public function toJson(){
+        #$data = array('storeItemID' => null, 'storeID' => $this->storeID, 'itemID' => $this->itemID, 'inStock' => $this -> inStock, 'storeItemQuantity' => $this -> storeItemQuantity);
+        $json = json_encode($this);
+        return $json;
+    }
+}
+class StoreInventoryController{
     public $url ="http://127.0.0.1:8000/";
 
     function __construct(){
-        print("A new SC has been created!\n");
+        print("A new SIC has been created!\n");
     }
-    #Inserts Store
-    public function insertStore($storeID,$storeLocation){
-        $store = new Store($storeID,$storeLocation);
-        $json = $store->toJson();
-        $path = $this->url."storeItem/storeID/".$storeID."/storeLocation/".$storeLocation;
+    #
+    public function createStoreItem($storeID,$itemID,$inStock,$storeItemQuantity){
+        $item = new Item($storeID,$itemID,$inStock,$storeItemQuantity);
+        $json = $item->toJson();
+        $path = $this->url."storeItem/storeID/".$storeID."/itemID/".$itemID."/inStock/".$inStock."/storeItemQuantity/".$storeItemQuantity;
         
         print("Path: ".$path." json: ".$json);
         
@@ -36,29 +55,58 @@ class StoreController{
         $result = curl_exec($ch);
 
         $data = json_decode($result,false);
-        print("Successfully added new item to Store!\n\n");
+        print("Successfully added new item to StoreInventory!");
     }
     
-    #Returns ID
-    public function getStoreID($storeLocation){
-        $path = $this->url."store/storeLocation/".$storeLocation;
+    #Returns dictionary
+    public function getItem($storeID,$itemID){
+        $path = $this->url."store/storeID"."/".$storeID."/itemID/".$itemID;
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $path);
         curl_setopt($ch,CURLOPT_CUSTOMREQUEST,'GET');
-        curl_setopt($ch,CURLOPT_POSTFIELDS,$storeLocation);
+        curl_setopt($ch,CURLOPT_POSTFIELDS,$storeID);
         curl_setopt($ch,CURLOPT_RETURNTRANSFER,TRUE);
         $result = curl_exec($ch);
         $data = json_decode($result,true);
-        echo("Returned: ".json_encode($data));
+        echo(json_encode($data));
         return $data;
 
     }
+    #Returns list of Dictionaries
+    public function loadStoreInventoryItems($storeID){
+        $path = $this->url."store/storeID"."/".$storeID;
 
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $path);
+        curl_setopt($ch,CURLOPT_CUSTOMREQUEST,'GET');
+        curl_setopt($ch,CURLOPT_POSTFIELDS,$storeID);
+        curl_setopt($ch,CURLOPT_RETURNTRANSFER,TRUE);
+        $result = curl_exec($ch);
+        $data = json_decode($result,true);
+        echo(json_encode($data));
+        return $data;
+    }
+    public function updateStoreItemQuantity($storeID,$itemID,$storeItemQuantity){
+        $item = new UpdateItem($storeID,$itemID,$storeItemQuantity);
+        $json = $item->toJson();
 
+        $path = $this->url."storeItem/storeID/".$storeID."/itemID/".$itemID."/storeItemQuantity/".$storeItemQuantity;
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $path);
+        curl_setopt($ch,CURLOPT_CUSTOMREQUEST,'PUT');
+        curl_setopt($ch,CURLOPT_POSTFIELDS,$json);
+        curl_setopt($ch,CURLOPT_RETURNTRANSFER,TRUE);
+        $result = curl_exec($ch);
+        $data = json_decode($result,true);
+        echo("returned: ".json_encode($data));
+        return $data;
+    }
 }
-$sic = new StoreController();
-    
-    $sic->insertStore(1,"address");
-    
-    $sic->getStoreID("address");
+$sic = new StoreInventoryController();
+    /* 
+    $sic->createStoreItem(1,1,'y',5);
+    */
+    $storeItems = $sic->loadStoreInventoryItems(1);
+    $sic->getItem(1,1);
+    $sic->updateStoreItemQuantity(1,1,50);
 ?>
