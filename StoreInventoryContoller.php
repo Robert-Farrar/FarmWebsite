ae717024137cd95b2b691e843c40557134a3507b
Tony Aldana <125922681+AldanaTA@users.noreply.github.com>
2025-03-08 16:05:07 -0600

    Update StoreInventoryContoller.php
    Finished StoreInventoryController

--- StoreInventoryContoller.php
+++ StoreInventoryContoller.php
@@ -1,38 +1,81 @@
+
 <?php
 class Item{
+    private $storeItemID = null;
     public $storeID = 0;
     public $itemID = 0;
     public $inStock = '';
     public $storeItemQuantity = 0;
-    function __contruct($storeID,$itemID,$inStock,$storeItemQuantity){
+    function __construct($storeID,$itemID,$inStock,$storeItemQuantity){
         $this->storeID = $storeID;
         $this->itemID = $itemID;
         $this->inStock = $inStock;
         $this->storeItemQuantity = $storeItemQuantity;
     }    
-    function toJson(){
-        return json_decode($this);
+    public function toJson(){
+        #$data = array('storeItemID' => null, 'storeID' => $this->storeID, 'itemID' => $this->itemID, 'inStock' => $this -> inStock, 'storeItemQuantity' => $this -> storeItemQuantity);
+        $json = json_encode($this);
+        return $json;
     }
 }
 class StoreInventoryController{
     public $url ="http://127.0.0.1:8000/";
 
-    function __contruct(){
+    function __construct(){
         print("A new SIC has been created!\n");
     }
-    function InsertStoreItem($storeID,$itemID,$inStock,$storeItemQuantity){
-        $item - new Item($storeID,$itemID,$inStock,$storeItemQuantity);
-        $ch = curl_init($ch);
-        curl_setopt($ch, CURLOPT_URL, $url."storeItem/");
-        curl_setopt($ch,CURLOPT_POSTFIELDS,$item.toJson());
+    #
+    public function createStoreItem($storeID,$itemID,$inStock,$storeItemQuantity){
+        $item = new Item($storeID,$itemID,$inStock,$storeItemQuantity);
+        $json = $item->toJson();
+        $path = $this->url."storeItem/storeID/".$storeID."/itemID/".$itemID."/inStock/".$inStock."/storeItemQuantity/".$storeItemQuantity;
+        
+        print("Path: ".$path." json: ".$json);
+        
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $path);
+        curl_setopt($ch,CURLOPT_POSTFIELDS,$json);
         curl_setopt($ch,CURLOPT_RETURNTRANSFER,TRUE);
         
         $result = curl_exec($ch);
 
         $data = json_decode($result,false);
-        print($data);
+        print("Successfully added new item to StoreInventory!");
+    }
+    
+    #Returns dictionary
+    public function getItem($storeID,$itemID){
+        $path = $this->url."store/storeID"."/".$storeID."/itemID/".$itemID;
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $path);
+        curl_setopt($ch,CURLOPT_CUSTOMREQUEST,'GET');
+        curl_setopt($ch,CURLOPT_POSTFIELDS,$storeID);
+        curl_setopt($ch,CURLOPT_RETURNTRANSFER,TRUE);
+        $result = curl_exec($ch);
+        $data = json_decode($result,true);
+        echo(json_encode($data));
+        return $data;
+
+    }
+    #Returns list of Dictionaries
+    public function loadStoreInventoryItems($storeID){
+        $path = $this->url."store/storeID"."/".$storeID;
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $path);
+        curl_setopt($ch,CURLOPT_CUSTOMREQUEST,'GET');
+        curl_setopt($ch,CURLOPT_POSTFIELDS,$storeID);
+        curl_setopt($ch,CURLOPT_RETURNTRANSFER,TRUE);
+        $result = curl_exec($ch);
+        $data = json_decode($result,true);
+        echo(json_encode($data));
+        return $data;
     }
 }
-    $sic = new StoreInventoryController();
-    $sic.InsertStoreItem(1,1,'y',5);
+$sic = new StoreInventoryController();
+    /* 
+    $sic->createStoreItem(1,1,'y',5);
+    */
+    $storeItems = $sic->loadStoreInventoryItems(1);
+    $sic->getItem(1,1)
 ?>
