b94b8c8f4c781afc92316d090c282770497026f3
Tony Aldana <125922681+AldanaTA@users.noreply.github.com>
2025-03-10 14:52:24 -0500

    Update StoreInventoryContoller.php

--- StoreInventoryContoller.php
+++ StoreInventoryContoller.php
@@ -1,98 +1,113 @@
-from fastapi import FastAPI, Depends, HTTPException
-from pydantic import BaseModel, Field
-from sqlalchemy import Column, Integer, String, create_engine, select
-from sqlalchemy.orm import Mapped,mapped_column,Session, DeclarativeBase, sessionmaker
-from typing import Optional
-from pymysql import install_as_MySQLdb
-from sqlalchemy_utils import database_exists, create_database
 
-install_as_MySQLdb()
+<?php
+class StoreItem{
+    private $storeItemID = null;
+    public $storeID = 0;
+    public $itemID = 0;
+    public $inStock = '';
+    public $storeItemQuantity = 0;
+    function __construct($storeID,$itemID,$inStock,$storeItemQuantity){
+        $this->storeID = $storeID;
+        $this->itemID = $itemID;
+        $this->inStock = $inStock;
+        $this->storeItemQuantity = $storeItemQuantity;
+    }    
+    public function toJson(){
+        #$data = array('storeItemID' => null, 'storeID' => $this->storeID, 'itemID' => $this->itemID, 'inStock' => $this -> inStock, 'storeItemQuantity' => $this -> storeItemQuantity);
+        $json = json_encode($this);
+        return $json;
+    }
+}
+class UpdateItem{
+    public $storeID = 0;
+    public $itemID = 0;
+    public $storeItemQuantity = 0;
+    function __construct($storeID,$itemID,$storeItemQuantity){
+        $this->storeID = $storeID;
+        $this->itemID = $itemID;
+        $this->storeItemQuantity = $storeItemQuantity;
+    }    
+    public function toJson(){
+        #$data = array('storeItemID' => null, 'storeID' => $this->storeID, 'itemID' => $this->itemID, 'inStock' => $this -> inStock, 'storeItemQuantity' => $this -> storeItemQuantity);
+        $json = json_encode($this);
+        return $json;
+    }
+}
+class StoreInventoryController{
+    public $url ="http://127.0.0.1:8000/";
 
-class Base(DeclarativeBase):
-    pass
+    function __construct(){
+        print("A new SIC has been created!\n");
+    }
+    #
+    public function createStoreItem($storeID,$itemID,$inStock,$storeItemQuantity){
+        $item = new StoreItem($storeID,$itemID,$inStock,$storeItemQuantity);
+        $json = $item->toJson();
+        $path = $this->url."storeItem/storeID/".$storeID."/itemID/".$itemID."/inStock/".$inStock."/storeItemQuantity/".$storeItemQuantity;
+        
+        print("Path: ".$path." json: ".$json);
+        
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $path);
+        curl_setopt($ch,CURLOPT_POSTFIELDS,$json);
+        curl_setopt($ch,CURLOPT_RETURNTRANSFER,TRUE);
+        
+        $result = curl_exec($ch);
 
-class Item(BaseModel):
-    storeItemID: Optional[int]
-    storeID: int
-    itemID: int 
-    inStock: str 
-    storeItemQuantity: int 
-
-
-class StoreInventoryItem(Base):
-    __tablename__ = "StoreInventory"
-    storeItemID: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
-    storeID:  Mapped[int] = mapped_column(index=True)
-    itemID:   Mapped[int] = mapped_column(Integer)
-    inStock: Mapped[str] = mapped_column(String(1))
-    storeItemQuantity: Mapped[int]= mapped_column(Integer)
+        $data = json_decode($result,false);
+    }
     
-mariadbCon = "mariadb://pmaUser:pma@127.0.0.1/StoreInventory"
-engine = create_engine(mariadbCon)
-if not database_exists(engine.url):
-    create_database(engine.url)
-    print("Database has been created!\n")
+    #Returns dictionary
+    public function getStoreItem($storeID,$itemID){
+        $path = $this->url."store/storeID"."/".$storeID."/itemID/".$itemID;
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $path);
+        curl_setopt($ch,CURLOPT_CUSTOMREQUEST,'GET');
+        curl_setopt($ch,CURLOPT_POSTFIELDS,$storeID);
+        curl_setopt($ch,CURLOPT_RETURNTRANSFER,TRUE);
+        $result = curl_exec($ch);
+        $data = json_decode($result,true);
+        return $data;
+
+    }
+    #Returns list of Dictionaries
+    public function loadStoreInventoryItems($storeID){
+        $path = $this->url."store/storeID"."/".$storeID;
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $path);
+        curl_setopt($ch,CURLOPT_CUSTOMREQUEST,'GET');
+        curl_setopt($ch,CURLOPT_POSTFIELDS,$storeID);
+        curl_setopt($ch,CURLOPT_RETURNTRANSFER,TRUE);
+        $result = curl_exec($ch);
+        $data = json_decode($result,true);
+        return $data;
+    }
+    public function updateStoreItemQuantity($storeID,$itemID,$storeItemQuantity){
+        $item = new UpdateItem($storeID,$itemID,$storeItemQuantity);
+        $json = $item->toJson();
+
+        $path = $this->url."storeItem/storeID/".$storeID."/itemID/".$itemID."/storeItemQuantity/".$storeItemQuantity;
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $path);
+        curl_setopt($ch,CURLOPT_CUSTOMREQUEST,'PUT');
+        curl_setopt($ch,CURLOPT_POSTFIELDS,$json);
+        curl_setopt($ch,CURLOPT_RETURNTRANSFER,TRUE);
+        $result = curl_exec($ch);
+        $data = json_decode($result,true);
+        echo("returned: ".json_encode($data));
+        return $data;
+    }
+ 
     
-sessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-def createDbAndTables():
-    Base.metadata.create_all(engine)
-
-def getSession():
-    db = sessionLocal()
-    try:
-        yield db
-    except:
-        print("failed to start session")
-        raise
-    finally:
-        db.close()
-
-
-app = FastAPI()
-@app.on_event("startup")
-def on_startup():
-    createDbAndTables()
-
-#Should be done
-@app.post("/storeItem/storeID/{storeID}/itemID/{itemID}/inStock/{inStock}/storeItemQuantity/{storeItemQuantity}")
-def insertStoreItem(storeID: int, itemID: int, inStock: str, storeItemQuantity:int,db:Session = Depends(getSession)):
-
-    item = StoreInventoryItem()
-    item.storeID = storeID
-    item.itemID = itemID
-    item.inStock = inStock
-    item.storeItemQuantity = storeItemQuantity
-   
-    db.add(item)
-    db.commit()
-    db.refresh(item)
-    return item
-
-#working
-@app.get("/store/storeID/{storeID}")
-def readStoreInventory(storeID:int,db:Session = Depends(getSession)):
-    stmt = select(StoreInventoryItem)
-    result = db.query(StoreInventoryItem).filter(StoreInventoryItem.storeID == storeID)
-    inventory = []
-    [inventory.append(row.__dict__) for row in result]
-    return inventory
+}
+#$sic = new StoreInventoryController();
     
-#working
-@app.get("/store/storeID/{storeID}/itemID/{itemID}")
-def getItem(storeID: int,itemID: int, db: Session = Depends(getSession)):
-    item = db.query(StoreInventoryItem).filter(StoreInventoryItem.storeID == storeID).filter(StoreInventoryItem.itemID == itemID).first()
-
-    return item.__dict__
-
-@app.put("/storeItem/storeID/{storeID}/itemID/{itemID}/storeItemQuantity/{storeItemQuantity}")
-def updateItemQty(storeID: int,itemID: int, storeItemQuantity: int, db: Session = Depends(getSession)):
-     item = db.query(StoreInventoryItem).filter(StoreInventoryItem.storeID == storeID).filter(StoreInventoryItem.itemID == itemID).first()
-     if storeItemQuantity == 0:
-        item.inStock = 'N'
-        item.storeItemQuantity =storeItemQuantity
-     else:
-        item.inStock = 'Y'
-        item.storeItemQuantity =storeItemQuantity
+    #$sic->createStoreItem(1,2,'y',15);
     
-     return item.__dict__
+    #$storeItems = $sic->loadStoreInventoryItems(1);
+    #$sic->getItem(1,1);
+    #$storeItems = $sic->loadStore(1,$sic);
+    #echo var_dump($storeItems);
+    #$sic->updateStoreItemQuantity(1,1,50);
+?>
