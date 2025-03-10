376244ebcdd7d935be41a9e1758f883ab1888ccd
Robert-Farrar <157628762+Robert-Farrar@users.noreply.github.com>
2025-03-09 20:10:27 -0500

    Add files via upload

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
 error_reporting(E_ALL);
+ini_set('display_errors', "1");
 require_once "CustomerController.php";
+require_once "OrderController.php";
 
 session_start();
 
@@ -20,23 +22,47 @@ if($action == "login"){
     $username = $_POST['username'];
     $password = $_POST['passwrd'];
    $id = $controller->checkCustomerCredentials($username, $password);
-   echo $id;
 
    if(isset($id)){
     $_SESSION['customerID'] = $id;
-    header("Location:home.php");
+    include "home.php";
    }
    else{
     header("Location:index.php");
    }
 
 }
+else if($action == "add"){
+    $controller = new CustomerController();
+    $username = $_POST['username'];
+    $password = $_POST['passwrd'];
+    $fullName = $_POST['fullName'];
+    $email = $_POST['email'];
+    $customerAddress = $_POST['customerAddress'];
+    $phoneNumber = $_POST['phoneNumber'];
 
-else if($_SESSION['customerID'] !== NULL){
+    $controller->insertCustomer($username, $password, $fullName, $email, $customerAddress, $phoneNumber);
+
+    header("Location:index.php");
+
+}
+
+else if($_SESSION['customerID'] !== NULL and $_REQUEST['action'] == "history"){
+    $id = $_SESSION['customerID'];
+    $controller = new OrderController();
+
+    $orders = $controller->getOrders($id);
+
+    include "OrderHistory.php";
+    exit;
+}
+else if($SESSION['customerID'] !== NULL and $_REQUEST['action'] == "checkout"){
+    include "Checkout.php";
     exit;
 }
 
 else{
+    
     include "login.php";
 }
 ?>
